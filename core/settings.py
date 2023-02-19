2f56d974638f2e3a029eb4aaa3615f1838982dd7
oswaldo Echeverria <oswaldo.echeverria@outlook.com>
2023-02-19 18:27:06 -0500

    Fix css y disenio de home

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATIC_URL = 'static/'
+STATIC_URL = 'core/static/'
 STATICFILES_DIRS = [ BASE_DIR  /  "static" ]  
 STATIC_ROOT = BASE_DIR  /  "staticfiles" 
 STATICFILES_STORAGE  =  "django.contrib.staticfiles.storage.StaticFilesStorage"
